cd9600767aabc7cbd6a516187cc1ee8d792d1dba
apt-9 <lachainedespectr0m@gmail.com>
2022-06-10 21:55:35 +0200

    apt

--- index.js
+++ index.js
@@ -74,15 +74,12 @@ client.on('ready', () => {
 
 
 })
-client.on('messageDelete', async function(message, bot){
-  if (message.author == null) return;
-  if(message.author.bot) return;
+client.snipes = new Map()
+client.on('messageDelete', function(message, channel){
   client.snipes.set(message.channel.id, {
     content: message.content,
-    author: message.author.tag,
-    member: message.member,
+    author: message.author.id,
     image: message.attachments.first() ? message.attachments.first().proxyURL : null,
-    avatar: message.author.avatarURL
   })
 })
 
